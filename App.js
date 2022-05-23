01eb910468c197805069bbfbc64830ad4fcd30f9
lim-kim930 <1625753207lim@gmail.com>
2022-05-23 13:04:20 +0800

    some updates

--- App.js
+++ App.js
@@ -5,7 +5,6 @@
  * @format
  * @flow strict-local
  */
-
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -16,97 +15,107 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  DynamsoftBarcodeReader,
+  DynamsoftCameraView,
+  BarcodeResult,
+  EnumDBRPresetTemplate,
+  Region,
+  EnumBarcodeFormat,
+  DBRRuntimeSettings
+} from 'henry-capture-vision-react-native';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}adwdawdawdawd
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+class App extends React.Component {
+  state = {
+    results: null 
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  componentDidMount() {
+    (async () => {
+        try {
+            await DynamsoftBarcodeReader.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9")
+        } catch (e) {
+            console.log(e)
+        }
+        this.reader = await DynamsoftBarcodeReader.createInstance();
+        await this.reader.updateDBRRuntimeSettings(EnumDBRPresetTemplate.DEFAULT);
+        let settings: DBRRuntimeSettings = await this.reader.getDBRRuntimeSettings();
+        // Set the expected barcode count to 0 when you are not sure how many barcodes you are scanning.
+        // Set the expected barcode count to 1 can maximize the barcode decoding speed.
+        settings.expectedBarcodesCount = 0;
+        settings.barcodeFormatIds = EnumBarcodeFormat.BF_ONED | EnumBarcodeFormat.BF_QR_CODE;
+        await this.reader.updateDBRRuntimeSettings(settings)
+
+        this.reader.addResultListener(async (results: BarcodeResult[]) => {
+            if(results.length>0)
+                await this.reader.stopScanning();
+            this.setState({results: results});
+            // console.log(results.length)
+        })
+    })();
+  };
+
+  async componentWillUnmount() {
+    await this.reader.stopScanning()
+    this.reader.removeResultListener()
+  };
+
+  render() {
+    let region: Region;
+    let barcode_text = "";
+    region = {
+        regionTop: 30,
+        regionLeft: 15,
+        regionBottom: 70,
+        regionRight: 85,
+        regionMeasuredByPercentage: true
+    }
+    let results: BarcodeResult[] = this.state.results;
+    if (results && results.length > 0){
+        for (var i=0; i<results.length; i++) {
+            barcode_text += results[i].barcodeFormatString+":"+results[i].barcodeText+"\n"
+        }
+        console.log(barcode_text);
+    }
+    const showScanner = async ()=> {
+        this.setState({results: []});
+        await this.reader.startScanning();
+    }
+
+    return (
+        (results && results.length <= 0)
+        ?<DynamsoftCameraView style={{flex: 1,}} ref = {(ref)=>{this.scanner = ref}} scanRegion ={region} overlayVisible={false}></DynamsoftCameraView>
+        :<View style={styles.container}>
+            <Text style={styles.result}>{results && results.length > 0 ? barcode_text : "Press button to start"}</Text>
+            <View style={styles.button}>
+                <Button onPress={showScanner} title="Start" />
+            </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+      flexDirection: "column",
+      justifyContent: "center",
+      flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  result: {
+    color: '#ccc',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: "center",
+    marginBottom: 10
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  button: {
+      color: "blue",
+      width:"50%",
+      marginLeft: "25%"
+  }
 });
 
 export default App;
